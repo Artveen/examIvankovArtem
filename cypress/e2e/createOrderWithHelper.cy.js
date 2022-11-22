fe0493d379f3c795de9f61aa90dce0ab52ab7926
Artem Ivankov <artem.ivankov.95@gmail.com>
2022-11-22 17:04:32 +0200

    helper added, feedback updated

--- cypress/e2e/createOrderWithHelper.cy.js
+++ cypress/e2e/createOrderWithHelper.cy.js
@@ -8,8 +8,8 @@ it('Search and order item from main page and order it', () => {
     cy.get('[aria-label="Close Welcome Banner"]').click();
     cy.get('[aria-label="Show/hide account menu"]').click();
     cy.get('button[aria-label="Go to login page"]').click();
-    cy.get('[aria-label="Text field for the login email"]').type('pap123@papa.com');
-    cy.get('[aria-label="Text field for the login password"]').type('pwd123@Aa');
+    cy.get('[aria-label="Text field for the login email"]').type('artem.ivankov.95@gmail.com');
+    cy.get('[aria-label="Text field for the login password"]').type('Qwerty123');
     cy.get('[aria-label="Login"]').click();
 
     cy.get('[aria-label="dismiss cookie message"]').click();
